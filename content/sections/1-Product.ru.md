eb5351e0710a1ef9b4d54734b404d5e4692d565b
Herman <balyba95@gmail.com>
2024-10-09 18:44:15 +0500

    feat: update product data, formatted and types for ProductItem & ProductDetailDialog

--- content/sections/1-Product.ru.md
+++ content/sections/1-Product.ru.md
@@ -4,57 +4,67 @@ header: "Товары"
 subheader: "в аренду"
 products: [
   {
+    imageFileName: "product/Артромот.jpg",
+    imageFileNameDetail: ["product/Артромот-full.jpg","product/Артромот-full2.jpeg"],
+    header: "Артромот",
+    subheader: "тренажер для пассивной разработки тазобедренного/коленного сустава (CPM терапия)",
+    subheaderOutside: "Бесплатная доставка, без залога.",
+    extraInfo: ["10 дней: 10.000₽","14 дней 14.000₽","30 дней 30.000₽",],
+    more: "Подробнее",
+    close: "Закрыть"
+  },
+  {
     imageFileName: "product/костыли.jpg",
-    imageFileNameDetail: "product/костыль-full.jpeg",
+    imageFileNameDetail: ["product/костыль-full.jpg"],
     header: "Костыли",
     subheader: "Взрослые и детские, на рост от 110 см.",
-    extraInfo: ["10 дней: 350₽", "15 дней: 450₽","месяц: 500₽, далее 12₽/сутки", "Залог: 500₽"],
+    extraInfo: ["10 дней: 500₽","месяц: 700₽, далее 12₽/сутки", "Залог: 500₽"],
     more: "Подробнее",
     close: "Закрыть"
   }, 
   {
     imageFileName: "product/ортез.jpg",
-    imageFileNameDetail: "product/ортез-full.jpg",
+    imageFileNameDetail: ["product/ортез-full.jpg", "product/ортез-full2.jpg", "product/ортез-full3.PNG"],
     header: "Ортезы",
     subheader: "Коленные, плечевые, тазобедренные и другие",
-    extraInfo: ["Ортез коленный:", " 10 дней: 1.250₽, 15 дней: 1.500₽,", "30 дней: 2.500₽.", "Ортез плечевой Абдукционный:", " 30 дней 2.900₽, далее 70₽/cутки "],
+    extraInfo: ["Ортез коленный,голеностопный:", " 10 дней: 1.250₽", "30 дней: 2.500₽.", "Ортез плечевой Абдукционный:", " 30 дней 3.700₽, далее 100₽/cутки "],
     more: "Подробнее",
     close: "Закрыть"
   },
   {
     imageFileName: "product/алмаг.jpg",
-    imageFileNameDetail: "product/алмаг-full.jpg",
+    imageFileNameDetail: ["product/алмаг-full.JPG","product/алмаг-full2.JPG"],
     header: "Алмаг",
     subheader: "Аппарат для проведения домашней магнитотерапии",
-    extraInfo: ["10 дней + доставка + вывоз: 1200₽", "14 дней 1200₽ далее 75₽/сутки"],
+    extraInfo: ["10 дней + доставка + вывоз: 1500₽", "14 дней 1500₽ далее 75₽/сутки"],
     more: "Подробнее",
     close: "Закрыть"
   },
   {
     imageFileName: "product/коляска.jpg",
-    imageFileNameDetail: "product/коляска-full.jpg",
+    imageFileNameDetail: ["product/коляска-full.jpg","product/коляска-full2.jpg"],
     header: "Инвалидные коляски",
     subheader: "Складные, с ручным приводом",
-    extraInfo: ["3 суток: 700₽","Далее: 100₽/сутки", "Депозит: 1000₽"],
+    extraInfo: ["2 суток: 600₽","Далее: 100₽/сутки"],
     more: "Подробнее",
     close: "Закрыть"
   },
   {
     imageFileName: "product/ходунки.jpg",
-    imageFileNameDetail: "product/ходунки-full.jpg",
+    imageFileNameDetail:["product/ходунки-full.jpg"],
     header: "Ходунки",
     subheader: "Легкие алюминиевые опоры для передвижения, с колёсиками и без",
-    extraInfo: ["месяц: 600₽", "Залог: 500₽"],
+    extraInfo: ["месяц: 700₽", "Залог: 500₽"],
     more: "Подробнее",
     close: "Закрыть"
   },
   {
     imageFileName: "product/концентратор.jpg",
-    imageFileNameDetail: "product/концентратор-full.jpg",
+    imageFileNameDetail: ["product/концентратор-full.jpg"],
     header: "Кислородный концентратор",
     subheader: "Насыщение кислородом, восстановление после COVID-19, объём 5 литров. Доставка, средства гигиены (канюля) входят в стоимость аренды.",
     subheaderOutside: "Насыщение кислородом, восстановление после COVID-19, объём 5 литров. Бесплатная доставка.",
-    extraInfo: ["2 недели: 5000₽", "месяц: 8500₽", "Доставка вывоз и канюля в подарок"],
+    extraInfo: ["1 неделя: 2500₽", "месяц: 6000₽", "Доставка, вывоз и канюля в подарок"],
     more: "Подробнее",
     close: "Закрыть"
   }, 
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -8,87 +8,86 @@ import ProductDetailDialog from "components/ProductDetailDialog";
 import "./ProductItem.scss";
 
 const ProductItem = ({
-    imageFileName,
-    imageAlt,
-    header,
-    subheader,
-    subheaderOutside,
-    more,
-    close,
-    content,
-    imageFileNameDetail,
-    imageAltDetail,
-    extraInfo,
+  imageFileName,
+  imageAlt,
+  header,
+  subheader,
+  subheaderOutside,
+  more,
+  close,
+  content,
+  imageFileNameDetail,
+  imageAltDetail,
+  extraInfo,
 }) => {
-    const [showDetail, setShowDetail] = React.useState(false);
-    const handleShowDetail = React.useCallback(() => {
-        setShowDetail(true);
-    }, []);
-    const handleHideDetail = React.useCallback(() => {
-        setShowDetail(false);
-    }, []);
+  const [showDetail, setShowDetail] = React.useState(false);
+  const handleShowDetail = React.useCallback(() => {
+    setShowDetail(true);
+  }, []);
+  const handleHideDetail = React.useCallback(() => {
+    setShowDetail(false);
+  }, []);
 
-    return (
-        <>
-            <Col sm={6} lg={4} className="product-item">
-                <Image
-                    className="product-img"
-                    fileName={imageFileName}
-                    alt={imageAlt || header || subheader}
-                />
-                <div className="product-caption">
-                    <h4>{header}</h4>
-                    {subheaderOutside ? <p className="text-muted mb-2">{subheaderOutside}</p> : <p className="text-muted mb-2">{subheader}</p>}
-                    <div>{extraInfo}</div>
-                    <Button
-                        variant="primary"
-                        data-toggle="modal"
-                        onClick={handleShowDetail}
-                    >
-                        {more}
-                    </Button>
-                </div>
-
-            </Col>
-            <ProductDetailDialog
-                show={showDetail}
-                onHide={handleHideDetail}
-                imageFileName={imageFileNameDetail || imageFileName}
-                imageAlt={imageAltDetail || imageAlt}
-                header={header}
-                subheader={subheader}
-                close={close}
-                content={content}
-                extraInfo={extraInfo}
-            />
-        </>
-    );
+  return (
+    <>
+      <Col sm={6} lg={4} className="product-item">
+        <Image
+          className="product-img"
+          fileName={imageFileName}
+          alt={imageAlt || header || subheader}
+        />
+        <div className="product-caption">
+          <h4>{header}</h4>
+          {subheaderOutside ? (
+            <p className="text-muted mb-2">{subheaderOutside}</p>
+          ) : (
+            <p className="text-muted mb-2">{subheader}</p>
+          )}
+          <div>{extraInfo}</div>
+          <Button variant="primary" data-toggle="modal" onClick={handleShowDetail}>
+            {more}
+          </Button>
+        </div>
+      </Col>
+      <ProductDetailDialog
+        show={showDetail}
+        onHide={handleHideDetail}
+        imageFileName={imageFileNameDetail || imageFileName}
+        imageAlt={imageAltDetail || imageAlt}
+        header={header}
+        subheader={subheader}
+        close={close}
+        content={content}
+        extraInfo={extraInfo}
+      />
+    </>
+  );
 };
 
 ProductItem.propTypes = {
-    imageFileName: PropTypes.string.isRequired,
-    imageAlt: PropTypes.string,
-    header: PropTypes.string.isRequired,
-    subheader: PropTypes.string,
-    subheaderOutside: PropTypes.string,
-    content: PropTypes.string,
-    more: PropTypes.string,
-    close: PropTypes.string,
-    imageFileNameDetail: PropTypes.string,
-    imageAltDetail: PropTypes.string,
-    extraInfo: PropTypes.any,
+  imageFileName: PropTypes.string.isRequired,
+  imageAlt: PropTypes.string,
+  header: PropTypes.string.isRequired,
+  subheader: PropTypes.string,
+  subheaderOutside: PropTypes.string,
+  content: PropTypes.string,
+  more: PropTypes.string,
+  close: PropTypes.string,
+  imageFileNameDetail: PropTypes.any,
+  imageAltDetail: PropTypes.string,
+  extraInfo: PropTypes.any,
 };
 
 ProductItem.defaultProps = {
-    imageAlt: "",
-    subheader: "",
-    subheaderOutside: "",
-    content: "",
-    more: "",
-    close: "",
-    imageFileNameDetail: "",
-    imageAltDetail: "",
-    extraInfo: null,
+  imageAlt: "",
+  subheader: "",
+  subheaderOutside: "",
+  content: "",
+  more: "",
+  close: "",
+  imageFileNameDetail: null,
+  imageAltDetail: "",
+  extraInfo: null,
 };
 
 export default ProductItem;
