a77b9014f91fa12be76ef2fd51e915b037814253
Herman <balyba95@gmail.com>
2024-10-09 18:39:46 +0500

    chore[Product.jsx]: hide specials and formatted

--- src/views/Sections/Product/Product.jsx
+++ src/views/Sections/Product/Product.jsx
@@ -9,60 +9,70 @@ import PageSection from "components/PageSection";
 import "./Product.scss";
 
 const Product = ({ className, frontmatter }) => {
-    if (!frontmatter) {
-        return null;
-    }
+  if (!frontmatter) {
+    return null;
+  }
 
-    const { anchor, header: rootHeader, subheader: rootSubHeader, products } = frontmatter;
+  const { anchor, header: rootHeader, subheader: rootSubHeader, products } = frontmatter;
 
-    return (
-        <PageSection className={clsx("product-section", className)} id={anchor}>
-            <Row>
-                <SectionHeader header={rootHeader} subheader={rootSubHeader} />
-            </Row>
-            <Row className="justify-content-center">
-                {products.map(
-                    ({ content, extraInfo, header, imageFileName, imageFileNameDetail, subheader, subheaderOutside, more, close }) => (
-                        <ProductItem
-                            key={header}
-                            imageFileName={imageFileName}
-                            header={header}
-                            subheader={subheader}
-                            subheaderOutside={subheaderOutside}
-                            more={more}
-                            close={close}
-                            content={content}
-                            imageFileNameDetail={imageFileNameDetail}
-                            extraInfo={
-                                <ul>
-                                    {extraInfo.map((ei) => (
-                                        <li key={ei}>{ei}</li>
-                                    ))}
-                                </ul>
-                            }
-                        />
-                    ),
-                )}
-            </Row>
-            <div className="mt-n3">
-                <p className="text-muted mb-2">Постоянно действующие акции:</p>
+  return (
+    <PageSection className={clsx("product-section", className)} id={anchor}>
+      <Row>
+        <SectionHeader header={rootHeader} subheader={rootSubHeader} />
+      </Row>
+      <Row className="justify-content-center">
+        {products.map(
+          ({
+            content,
+            extraInfo,
+            header,
+            imageFileName,
+            imageFileNameDetail,
+            subheader,
+            subheaderOutside,
+            more,
+            close,
+          }) => (
+            <ProductItem
+              key={header}
+              imageFileName={imageFileName}
+              header={header}
+              subheader={subheader}
+              subheaderOutside={subheaderOutside}
+              more={more}
+              close={close}
+              content={content}
+              imageFileNameDetail={imageFileNameDetail}
+              extraInfo={
                 <ul>
-                    <li>Ортез и костыли на месяц за 2800 руб. с бесплатной доставкой</li>
-                    <li>Алмаг на 10 дней за 1000 руб. с бесплатной доставкой</li>
+                  {extraInfo.map((ei) => (
+                    <li key={ei}>{ei}</li>
+                  ))}
                 </ul>
-            </div>
-        </PageSection>
-    );
+              }
+            />
+          ),
+        )}
+      </Row>
+      {/* <div className="mt-n3">
+        <p className="text-muted mb-2">Постоянно действующие акции:</p>
+        <ul>
+          <li>Ортез и костыли на месяц за 2900 руб. с бесплатной доставкой и вывозом</li>
+          <li>Алмаг на 10 дней за 1000 руб. с бесплатной доставкой</li>
+        </ul>
+      </div> */}
+    </PageSection>
+  );
 };
 
 Product.propTypes = {
-    className: PropTypes.string,
-    frontmatter: PropTypes.object,
+  className: PropTypes.string,
+  frontmatter: PropTypes.object,
 };
 
 Product.defaultProps = {
-    className: null,
-    frontmatter: null,
+  className: null,
+  frontmatter: null,
 };
 
 export default Product;
